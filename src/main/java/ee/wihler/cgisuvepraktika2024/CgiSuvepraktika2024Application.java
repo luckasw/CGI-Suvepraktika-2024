7fc13044973c0a043eb05e5bb1999e20e7504370
luckasw <Albert.Wihler@gmail.com>
2024-03-02 19:38:52 +0200

    Initial commit

---
+++ src/main/java/ee/wihler/cgisuvepraktika2024/CgiSuvepraktika2024Application.java
@@ -0,0 +1,13 @@
+package ee.wihler.cgisuvepraktika2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiSuvepraktika2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgiSuvepraktika2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/ee/wihler/cgisuvepraktika2024/CgiSuvepraktika2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.wihler.cgisuvepraktika2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiSuvepraktika2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
