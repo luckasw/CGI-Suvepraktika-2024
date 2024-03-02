babe39673db5364edd4170aeca1bf1d4aa5a3e25
luckasw <Albert.Wihler@gmail.com>
2024-03-02 19:45:32 +0200

    V1 sql creation

---
+++ src/main/resources/db/migration/V1_initialSQL.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Genres (
+                        GenreID INT PRIMARY KEY,
+                        GenreName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Directors (
+                           DirectorID INT PRIMARY KEY,
+                           DirectorName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Movies (
+                        MovieID INT PRIMARY KEY,
+                        Title VARCHAR(255) NOT NULL,
+                        ReleaseDate DATE NOT NULL,
+                        GenreID INT NOT NULL,
+                        DirectorID INT NOT NULL,
+                        AgeRestriction INT NOT NULL,
+                        Language VARCHAR(255) NOT NULL,
+                        Runtime TIME NOT NULL,
+                        FOREIGN KEY (GenreID) REFERENCES Genres(GenreID),
+                        FOREIGN KEY (DirectorID) REFERENCES Directors(DirectorID)
+);
+
+CREATE TABLE Users (
+                       UserID INT PRIMARY KEY,
+                       UserName VARCHAR(255) UNIQUE NOT NULL,
+                       Email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE UserMovies (
+                            USERMOVIEID INT PRIMARY KEY,
+                            UserID INT,
+                            MovieID INT,
+                            Rating INT,
+                            FOREIGN KEY (UserID) REFERENCES Users(UserID),
+                            FOREIGN KEY (MovieID) REFERENCES Movies(MovieID),
+                            PRIMARY KEY (UserID, MovieID),
+                            CONSTRAINT ratingRangeCheck CHECK (Rating >= 1 and Rating <= 5)
+);
+
+CREATE TABLE Screenings (
+                            screening_id INT PRIMARY KEY,
+                            MovieID INT NOT NULL,
+                            screening_time TIMESTAMP NOT NULL,
+                            FOREIGN KEY (MovieID) REFERENCES Movies(MovieID)
+);
