e79df0f3e5486809e69ae34966e824de66ba1b90
luckasw <Albert.Wihler@gmail.com>
2024-03-11 20:57:29 +0200

    Deleted UsermovieId entity

--- src/main/java/ee/wihler/cgisuvepraktika2024/repositories/UsermovieRepository.java
+++ src/main/java/ee/wihler/cgisuvepraktika2024/repositories/UsermovieRepository.java
@@ -1,7 +1,6 @@
 package ee.wihler.cgisuvepraktika2024.repositories;
 
 import ee.wihler.cgisuvepraktika2024.entities.Usermovie;
-import ee.wihler.cgisuvepraktika2024.entities.UsermovieId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
